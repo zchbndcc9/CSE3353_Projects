fa62ec2702ce5e0dd0e6461ee4942e9d7a223826
Zachary Banducci <zbducci@gmail.com>
2018-02-18 22:04:41 -0600

    Added quicksort funcitons to own file

---
+++ Project1/quicksort.js
@@ -0,0 +1,30 @@
+const quickSort = (arr, compareFxn = (left, right) => left > right) => {
+        return _quickSort(arr, 0, arr.length - 1, compareFxn);
+}
+
+const _quickSort = (arr, low, high, compareFxn) => {
+    if (low > high)
+        return;
+
+    let part = _partition(arr, low, high, compareFxn);
+    _quickSort(arr, low, part - 1, compareFxn);
+    _quickSort(arr, part + 1, high, compareFxn);
+}
+
+const _partition = (arr, low, high, compareFxn) => {
+    //Instatiate pivot value
+    swap(arr, Math.floor((low + high)/2), high);
+
+    let pivot, left;
+    pivot = arr[high];
+    left = low;
+
+    for(let i = low; i < high; i++) {
+        if(compareFxn(pivot, arr[i])) { //left[y] > right[y]
+            swap(arr, i, left++);
+        }
+    }
+    
+    swap(arr, high, left);
+    return left;
+}
