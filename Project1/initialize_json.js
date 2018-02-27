b4d59f904ab6d40f1378f9a9dc2863fb6bfbecf4
Zachary Banducci <zbducci@gmail.com>
2018-02-27 11:27:23 -0600

    Airline map initialization

--- Project1/initialize_json.js
+++ Project1/initialize_json.js
@@ -11,22 +11,23 @@ const options = {
     min: -10,
     max: 10,
     fixed: 4,
-    flights: 100000
-}
+    flights: 10000
+};
 
 chance.mixin({
     "flight": function(){
         return {
             "flightNumber": 
-                chance.string({ length: 2, alpha: true, casing: 'upper' }) 
-                + chance.pad(chance.integer({ min: 0, max: 9999 }), 4, 0),
+                chance.string({ length: 2, alpha: true, casing: 'upper' }) +
+                chance.pad(chance.integer({ min: 0, max: 9999 }), 4, 0),
             "x": chance.floating(options),
-            "y": chance.floating(options)
+            "y": chance.floating(options),
+            "heading": chance.integer({min: 0, max: 360})
         };
     }
-})
+});
 
-let flights = []
+let flights = [];
 for(let i = 0; i < options.flights; i++){
     //Name
     flights.push(chance.flight());
