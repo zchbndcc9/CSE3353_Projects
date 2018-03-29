ba45e5fb1cff6c6257e775c67a0586dc4adefdd8
Zach Banducci <zbducci@gmail.com>
2018-03-28 19:25:13 -0500

    Fixed methods and exported module

--- Project2/min-heap.js
+++ Project2/min-heap.js
@@ -1,42 +1,61 @@
-function heapify(data) {
+module.exports.heapify = function(data) {
 
     bubbleup = () => {
-        for(let i = heap.length; i >= 0; i --) {
+        for(let i = self.heap.length; i >= 0; i --) {
             bubbledown(i);
         }
     };
     
     bubbledown = parent => {
-        if(parent * 2 > heap.length) return;
+        if(parent * 2 > self.heap.length) return;
 
         let leftChild = getLeftChild(parent);
         let rightChild = getRightChild(parent);
 
-        let child = heap[leftChild] < heap[rightChild] ? leftChild : rightChild;
+        let child = self.heap[leftChild] < self.heap[rightChild] ? leftChild : rightChild;
 
-        if (heap[child] < heap[parent]) {
+        if (self.heap[child] < self.heap[parent]) {
           swap(child, parent);
           bubbledown(child);
         }
     };
     
     swap = (i, j) => {
-        let tmp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = tmp;
+        let tmp = self.heap[i];
+        self.heap[i] = self.heap[j];
+        self.heap[j] = tmp;
     };
 
     getLeftChild = index => index * 2;
 
     getRightChild = index => {
         let childIndex = index * 2 + 1;
-        return childIndex < heap.length ? childIndex : childIndex - 1;
+        return childIndex < self.heap.length ? childIndex : childIndex - 1;
         //Returns index of left child if there is no rightchild
     };
 
-    let heap = data.slice();
-    
+    pop = () => {
+        if(self.heap.length == 0) {
+            console.log("Cannot pop from empty self.heap");
+            return;
+        }
+        swap(0, self.heap.length - 1);
+        let result = self.heap.pop();
+        return result;
+    }
+
+    insert = member => {
+        self.heap.push(member);
+        bubbleup();
+    };
+
+    let self = {
+        heap: data.slice(),
+        pop: pop,
+        insert: insert
+    };
+
     bubbleup();
 
-    return heap;
+    return self;
 }
